de8c762826f8d2a7ecb351edea766e6d1d3bfefb
Labrock <vladimirpeshkov5@yandex.ru>
2025-04-22 14:27:05 +0300

    init menu-item

---
+++ src/resources/views/components/menu-item.blade.php
@@ -0,0 +1,22 @@
+@php
+    $categoryActive = in_array(Route::currentRouteName(), [
+        "admin.categories.index", "admin.categories.show"
+    ]);
+    $productActive = in_array(Route::currentRouteName(), [
+        "admin.products.index", "admin.products.show"
+    ]);
+    $specificationActive = in_array(Route::currentRouteName(), [
+        "admin.specifications.index"
+    ]);
+    $specificationGroupActive = in_array(Route::currentRouteName(), [
+       "admin.specification-groups.index"
+    ]);
+@endphp
+
+@can("viewAny", config("category-product.customCategoryModel") ?? \GIS\CategoryProduct\Models\Category::class)
+    <x-tt::admin-menu.item href="{{ route('admin.categories.index') }}"
+                           :active="$categoryActive">
+        <x-slot name="ico"><x-cp::ico.tree /></x-slot>
+        Категории товаров
+    </x-tt::admin-menu.item>
+@endcan
