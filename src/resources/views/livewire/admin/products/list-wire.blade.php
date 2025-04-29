6d6b930a9f2cd0f85500ad13b509fedf4575fc90
Labrock <vladimirpeshkov5@yandex.ru>
2025-04-29 16:13:34 +0300

    make product modal

--- src/resources/views/livewire/admin/products/list-wire.blade.php
+++ src/resources/views/livewire/admin/products/list-wire.blade.php
@@ -1,3 +1,13 @@
 <div>
-     The whole world belongs to you.
+     <div class="card">
+         <div class="card-body">
+             <div class="space-y-indent-half">
+                 @include("cp::admin.products.includes.search")
+                 <x-tt::notifications.error prefix="product-" />
+                 <x-tt::notifications.success prefix="product-" />
+             </div>
+         </div>
+         @include("cp::admin.products.includes.table")
+         @include("cp::admin.products.includes.table-modals")
+     </div>
 </div>
