c263488e823aa8a52c755e68db2de95e3d1c344e
Labrock <vladimirpeshkov5@yandex.ru>
2025-10-02 11:45:01 +0300

    make switch card style class

--- src/resources/views/livewire/web/catalog/product-list-wire.blade.php
+++ src/resources/views/livewire/web/catalog/product-list-wire.blade.php
@@ -1,14 +1,38 @@
-<div x-data="{ gridView: '{{ $gridView }}' }"
-     x-init="
-        $refs.listButton.classList.remove('text-primary', 'text-primary/25');
-        $refs.cardButton.classList.remove('text-primary', 'text-primary/25');
-        ">
+<div x-data="{
+        gridView: '',
+        chooseView(view) {
+            if (view === 'list' && this.gridView !== 'list') {
+                this.changeActive('list', this.$refs.listButton, this.$refs.cardButton);
+                this.$refs.productRow.classList.remove('card-style');
+            } else if (view === 'card' && this.gridView !== 'card') {
+                this.changeActive('card', this.$refs.cardButton, this.$refs.listButton);
+                this.$refs.productRow.classList.add('card-style');
+            }
+        },
+        changeActive(view, active, inactive) {
+            this.gridView = view;
+            this.$dispatch('change-product-grid-view', { view: view })
+            this.makeActive(active);
+            this.makeInactive(inactive);
+        },
+        makeActive(el) {
+            this.clearClass(el);
+            el.classList.add('text-primary');
+        },
+        makeInactive(el) {
+            this.clearClass(el);
+            el.classList.add('text-primary/25');
+        },
+        clearClass(el) {
+            el.classList.remove('text-primary', 'text-primary/25')
+        }
+    }" x-init="gridView = '{{ $gridView }}'">
     @include("cp::web.catalog.includes.product.controls")
 
     @if (! $products->count())
         <div>По вашему запросу ничего не найдено</div>
     @else
-        <div class="row product-row card-style">
+        <div class="row product-row {{ $gridView === 'card' ? 'card-style' : '' }}" x-ref="productRow">
             @foreach($products as $item)
                 @php($product = \GIS\CategoryProduct\Facades\ProductActions::getTeaserData($item->id))
                 <div class="col product-col">
--- src/resources/views/web/catalog/includes/sort-choosing.blade.php
+++ src/resources/views/web/catalog/includes/sort-choosing.blade.php
@@ -14,14 +14,11 @@
 
     <div class="flex items-center justify-end space-x-indent-half" wire:ignore>
         {{-- При изменении класса цвета кнопок, нужно их обнулить в product-list-wire --}}
-        <button type="button" @click="gridView = 'list'; $dispatch('change-product-grid-view', { view: 'list' })"
-                {{-- TODO: move to function --}}
-                :class="gridView === 'list' ? 'text-primary' : 'text-primary/25'" x-ref="listButton"
+        <button type="button" @click="chooseView('list')" x-ref="listButton"
                 class="cursor-pointer {{ $gridView == 'list' ? 'text-primary' : 'text-primary/25' }} hover:text-primary-hover">
             <x-cp::ico.list />
         </button>
-        <button type="button" @click="gridView = 'card'; $dispatch('change-product-grid-view', { view: 'card' })"
-                :class="gridView === 'list' ? 'text-primary/25' : 'text-primary'" x-ref="cardButton"
+        <button type="button" @click="chooseView('card')" x-ref="cardButton"
                 class="cursor-pointer {{ $gridView == 'list' ? 'text-primary/25' : 'text-primary' }} hover:text-primary-hover">
             <x-cp::ico.grid />
         </button>
