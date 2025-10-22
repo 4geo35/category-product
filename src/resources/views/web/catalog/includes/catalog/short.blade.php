4b465db47cc6d82b5cf83d8d88fad981f2d758a6
Labrock <vladimirpeshkov5@yandex.ru>
2025-10-22 17:17:47 +0300

    change info ico

--- src/resources/views/web/catalog/includes/catalog/short.blade.php
+++ src/resources/views/web/catalog/includes/catalog/short.blade.php
@@ -1,8 +1,8 @@
 @if ($category->short)
     <div class="ml-indent relative self-end" x-data="{ hovered: false, show: false }" @mouseleave="hovered = false" @click.outside="show = false">
         <button type="button" @mouseover="hovered = true" @click="show = true"
-                class="flex items-center justify-center size-[30px] pr-1 font-bold italic text-2xl text-white bg-[#E8E8E8] rounded-lg cursor-help">
-            i
+                class="flex items-center justify-center text-white w-[30px] h-[30px] bg-[#E8E8E8] rounded-lg cursor-help">
+            <x-cp::ico.info height="22" width="auto" />
         </button>
         <div class="bg-[#F4F4F4] z-10 absolute right-4 top-4 min-w-[270px] px-indent py-indent-half rounded-base shadow-md"
              x-show="hovered || show" style="display: none">
